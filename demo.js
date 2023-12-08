7b6d3811449cf91a2a91184fde8a1b637f2ae7e6
Garvit <dhameliyagarvit@gmail.com>
2023-12-08 09:08:56 +0530

    async await complate

---
+++ demo.js
@@ -0,0 +1,8 @@
+import "regenerator-runtime/runtime";
+import axios from "axios";
+// fetch("https://jsonplaceholder.typicode.com/todos",).then((res) => {
+//   //   console.log(res.json());
+//   res.json().then((result) => {
+//     console.log(result);
+//   });
+// });
